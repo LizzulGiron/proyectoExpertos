c453731b78b8d6035a4354e069ac04f018ab7ab6
LizzulGiron <liizzgiron@gmail.com>
2019-09-02 19:00:53 -0600

    actualizacion

--- public/JS/archives.js
+++ public/JS/archives.js
@@ -7,7 +7,7 @@ var imagenes = [
 	];
 
 var videos = [
-		"img/archives/img/photo5.jpg","img/archives/img/photo5.jpg",
+		"img/archives/img/photo4.jpg","img/archives/img/photo5.jpg",
 		"img/archives/img/photo7.jpg","img/archives/img/photo8.jpg",
 		"img/archives/img/photo9.jpg"
 	];
@@ -15,14 +15,16 @@ var videos = [
 (function(){
 	for (var i = 0; i < imagenes.length; i++) {
 		document.getElementById('tab1').innerHTML += `
-        			<div class="col-md-4 col-lg-3 col-sm-6 col-xs-6">
+        			<div class="col-md-4 col-lg-3 col-sm-6 col-xs-6" style="padding-left:0px;padding-right:0px;">
                         <div class="item">
-                            <span class="icono"><i class="fas fa-images"></i></span>
-                            <span class="icono"><i class="fas fa-trash-alt"></i></span>
                             <div class="image">
                                 <img src="${imagenes[i]}" alt="" class="img-fluid">
                                 <div class="featured-button button">
-                                    <a href="#projects">Codigo</a>
+                                    <a href="#projects">
+                                        Codigo
+                                        <span class="icono"><i class="fas fa-images"></i></span>
+                                    </a>
+
                                 </div>
                             </div>
                         </div>
@@ -31,14 +33,15 @@ var videos = [
 
     for (var i = 0; i < videos.length; i++) {
         document.getElementById('tab2').innerHTML += `
-                    <div class="col-md-4 col-lg-3 col-sm-6 col-xs-6">
+                    <div class="col-md-4 col-lg-3 col-sm-6 col-xs-6" style="padding-left:0px;padding-right:0px;">
                         <div class="item">
-                            <span class="icono"><i class="fas fa-video"></i></span>
-                            <span class="icono"><i class="fas fa-trash-alt"></i></span>
                             <div class="image">
                                 <img src="${videos[i]}" alt="" class="img-fluid">
                                 <div class="featured-button button">
-                                    <a href="#projects">Codigo</a>
+                                    <a href="#projects">
+                                        Codigo
+                                        <span class="icono"><i class="fas fa-video"></i></span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
